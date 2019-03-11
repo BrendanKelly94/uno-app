a983d55c32eccea9850869c44dc5d02a48d80414
Your Name <brendan.kelly94@gmail.com>
2019-03-11 11:18:18 -0700

    configuring seed file

--- src/backend/seeds/2_user_seed.js
+++ src/backend/seeds/2_user_seed.js
@@ -4,14 +4,14 @@ const queries = require('../db/queries')
 //name all passwords test for simplicity when testing
 const users = [
   {name: 'bot', pwd: 'bot'},
-  {name: 'brendan', pwd: 'test'}, //used for setting up game without bots
-  {name: 'brendan1', pwd: 'test'}, // ""
-  {name: 'brendan2', pwd: 'test'}, // ""
-  {name: 'bren', pwd: 'test'},  //used for setting up game with bots
-  {name: 'bren1', pwd: 'test'}, //used for testing join game endpoints
-  {name: 'bren2', pwd: 'test'}, //""
-  {name: 'bren3', pwd: 'test'}, //""
-  {name: 'test', pwd: 'test'}
+  // {name: 'brendan', pwd: 'test'}, //used for setting up game without bots
+  // {name: 'brendan1', pwd: 'test'}, // ""
+  // {name: 'brendan2', pwd: 'test'}, // ""
+  // {name: 'bren', pwd: 'test'},  //used for setting up game with bots
+  // {name: 'bren1', pwd: 'test'}, //used for testing join game endpoints
+  // {name: 'bren2', pwd: 'test'}, //""
+  // {name: 'bren3', pwd: 'test'}, //""
+  // {name: 'test', pwd: 'test'}
 ]
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries

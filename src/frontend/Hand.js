ee55ad223b72662a3eed9d346be3490747926eb1
BrendanKelly94 <brendan.kelly94@gmail.com>
2019-03-18 17:57:08 -0700

    fixing issue with NewCard blocking draw box

--- src/frontend/Hand.js
+++ src/frontend/Hand.js
@@ -35,7 +35,8 @@ const Hand = React.memo(({ tl, myId, hand, isMyTurn, lastTurnId, scaleFactor, dr
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'grey'
+      backgroundColor: 'grey',
+      zIndex: 10
   }
 
   const drawBoxStyle = {
@@ -198,10 +199,10 @@ const Hand = React.memo(({ tl, myId, hand, isMyTurn, lastTurnId, scaleFactor, dr
     {
      colorChange.status?
      <div style = {cChangeContainerStyle}>
-       <Card callback = {handleColorChange} cId = 'color-red' style = {{backgroundColor: 'red', zIndex: 100}}/>
-       <Card callback = {handleColorChange} cId = 'color-blue' style = {{backgroundColor: 'blue', zIndex: 100}}/>
-       <Card callback = {handleColorChange} cId = 'color-green' style = {{backgroundColor: 'green', zIndex: 100}}/>
-       <Card callback = {handleColorChange} cId = 'color-yellow' style = {{backgroundColor: 'yellow', zIndex: 100}}/>
+       <Card callback = {handleColorChange} cId = 'color-red' style = {{backgroundColor: 'red', zIndex: 10}}/>
+       <Card callback = {handleColorChange} cId = 'color-blue' style = {{backgroundColor: 'blue', zIndex: 10}}/>
+       <Card callback = {handleColorChange} cId = 'color-green' style = {{backgroundColor: 'green', zIndex: 10}}/>
+       <Card callback = {handleColorChange} cId = 'color-yellow' style = {{backgroundColor: 'yellow', zIndex: 10}}/>
      </div>
      :null
    }

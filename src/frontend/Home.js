4ed5b66de4c8388c64c285888ba1e25386c12f60
BrendanKelly94 <brendan.kelly94@gmail.com>
2019-04-15 11:52:49 -0700

    added svg circles to home view

--- src/frontend/Home.js
+++ src/frontend/Home.js
@@ -11,9 +11,17 @@ function Home(){
   const [ create, setCreate ] = useState(false);
   const [ context, setContext ] = useContext(authStoreContext);
 
+  const colorEnum = {
+    "red": "#FF4747",
+    "yellow": '#FFFF87',
+    "green": '#ACDB85',
+    "blue": '#84BAB7'
+  }
+
+
   const headingStyle = {
     textAlign: 'center',
-    marginBottom: '2em',
+    marginBottom: '1em',
     marginTop: '2em',
     fontSize: '4em',
     fontFamily: `Quicksand, sans-serif`,
@@ -55,6 +63,13 @@ function Home(){
     justifyContent: 'space-evenly'
   }
 
+  const svgContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    minHeight: '5em'
+  }
+
   const handleJoin = () => {
     if(context.user_name){
       history.push('/gamesList')
@@ -87,6 +102,14 @@ function Home(){
         </div>
         <div style = {homeContainerStyle}>
           <p style = {headingStyle}>UNO</p>
+
+          <div style = {svgContainerStyle}>
+            <svg height = '1em' width = '1em'> <circle r = '.5em' cx = {'50%'} cy = {'50%'} fill = {colorEnum['red']}/></svg>
+            <svg height = '1em' width = '1em'><circle r = '.5em' cx = {'50%'} cy = {'50%'} fill = {colorEnum['yellow']}/></svg>
+            <svg height = '1em' width = '1em'><circle r = '.5em' cx = {'50%'} cy = {'50%'} fill = {colorEnum['blue']}/>></svg>
+            <svg height = '1em' width = '1em'><circle r = '.5em' cx = {'50%'} cy = {'50%'} fill = {colorEnum['green']}/></svg>
+          </div>
+
           <div style = {buttonContainerStyle}>
             <Button id = 'join-button' variant = "default" color = "#C9DBBA" onClick = {handleJoin}>JOIN GAME</Button>
             <Button id = 'create-button' variant = "default" color = "#C9DBBA" onClick = {handleCreate}>CREATE GAME</Button>

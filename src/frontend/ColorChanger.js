4f663cc93e58449a1e32f49706579122fef3f2e2
BrendanKelly94 <brendan.kelly94@gmail.com>
2019-04-16 11:08:15 -0700

    adjusted color changer style

--- src/frontend/ColorChanger.js
+++ src/frontend/ColorChanger.js
@@ -3,6 +3,12 @@ import Card from './Card.js';
 
 function ColorChanger({changeColor, toggle, handRef}){
   const colors = ['red','blue', 'green', 'yellow'];
+  const colorEnum = {
+    "red": "#FF4747",
+    "yellow": '#FFFF87',
+    "green": '#ACDB85',
+    "blue": '#84BAB7',
+  }
   const containerRef = useRef(null);
   const containerStyle = {
       position: 'absolute',
@@ -22,8 +28,8 @@ function ColorChanger({changeColor, toggle, handRef}){
     zIndex: 10,
     width: '3em',
     height: '3em',
-    borderRadius: '50%'
-
+    borderRadius: '50%',
+    border: '2px solid #fff'
   }
 
   function mouseEnter(e){
@@ -47,7 +53,7 @@ function ColorChanger({changeColor, toggle, handRef}){
     <React.Fragment>
       <div style = {containerStyle} ref = {containerRef}>
         {colors.map((color) =>
-          <div key = {color} id = {color} onClick = {() => changeColor(color)} onMouseEnter = {mouseEnter} onMouseLeave = {mouseLeave} style = {{...selectionStyle, backgroundColor: color}}></div>
+          <div key = {color} id = {color} onClick = {() => changeColor(color)} onMouseEnter = {mouseEnter} onMouseLeave = {mouseLeave} style = {{...selectionStyle, backgroundColor: colorEnum[color]}}></div>
         )}
       </div>
 

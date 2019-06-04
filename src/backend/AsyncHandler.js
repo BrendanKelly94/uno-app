4a21edf7dc5baed21db15205f5faeef032211d6f
BrendanKelly94 <brendan.kelly94@gmail.com>
2019-06-04 10:07:12 -0700

    removed case where there is no (res, req, next)

--- src/backend/AsyncHandler.js
+++ src/backend/AsyncHandler.js
@@ -1,13 +1,7 @@
 const AsyncHandler = fn => (res, req, next) => {
-  if(!res){
-    Promise.resolve(fn()).catch(err => {
-      next(err);
-    })
-  }else{
     Promise.resolve(fn(res, req, next)).catch(err => {
       next(err);
     })
-  }
 }
 
 module.exports = AsyncHandler;
